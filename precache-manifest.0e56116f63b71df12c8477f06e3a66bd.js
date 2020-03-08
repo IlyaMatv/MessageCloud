7a3d2790b95d4658a21397ba4886cbf8c8de3378
Ilya Matv <arkashabi4@gmail.com>
2020-03-08 17:54:36 +0300

    Updates

---
+++ precache-manifest.0e56116f63b71df12c8477f06e3a66bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fca987d7d6a18252c0fc57373ed878f",
+    "url": "/MessageCloud/index.html"
+  },
+  {
+    "revision": "7c38bc9f5e16061c1db0",
+    "url": "/MessageCloud/static/css/main.18a558bf.chunk.css"
+  },
+  {
+    "revision": "87377ef9fa67899b9d9b",
+    "url": "/MessageCloud/static/js/2.895407e7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/MessageCloud/static/js/2.895407e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c38bc9f5e16061c1db0",
+    "url": "/MessageCloud/static/js/main.c971257b.chunk.js"
+  },
+  {
+    "revision": "df47a8acec30fb4a6374",
+    "url": "/MessageCloud/static/js/runtime-main.2710b7a5.js"
+  }
+]);
