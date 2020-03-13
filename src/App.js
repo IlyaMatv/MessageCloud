02bf5d8a36ccb565f2daecf459b01a2d097950c8
Ilya Matv <arkashabi4@gmail.com>
2020-03-13 15:02:30 +0300

    add hm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import MessageCloud from './components/MessageCloud/MessageCloud';
+import NameCmp from './components/NameCmp/NameCmp';
+
 
 function App() {
   return (
-    <div>
+    <div className="wrap">
 
-    <MessageCloud />
+      <NameCmp />
+      <MessageCloud />
 
     </div>
   );
---
+++ src/components/NameCmp/NameCmp.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import classes from './NameCmp.module.css'
+
+
+
+const NameCmp = (props) => {
+
+    const allQualities = ['буддист', 'контрабандист', 'тракторист']
+    const qualities = allQualities.map((el, index) => {
+        return (<div key={index} className={classes.nameItem}>{el}</div>)
+    })
+
+    return (
+        <div className={classes.name}>
+            <div>Матвейчук Илья</div>
+            {qualities}
+        </div>
+    )
+}
+
+export default NameCmp
